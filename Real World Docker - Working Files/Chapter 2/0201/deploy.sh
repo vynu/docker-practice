d239fc4310aa5406233de2f0d5b380ed4a60df4f
vynu <vynu@live.com>
2017-08-17 19:15:13 -0400

    added --net app (which will run in *app* docker network)

--- Real World Docker - Working Files/Chapter 2/0201/deploy.sh
+++ Real World Docker - Working Files/Chapter 2/0201/deploy.sh
@@ -8,5 +8,5 @@ docker stop web || true
 docker rm web || true
 docker rmi vynu/sample-node:current || true
 docker tag vynu/sample-node:latest vynu/sample-node:current
-docker run -d --restart always --name web -p 3000:3000 vynu/sample-node:current
+docker run -d --net app --restart always --name web -p 3000:3000 vynu/sample-node:current
 EOF
