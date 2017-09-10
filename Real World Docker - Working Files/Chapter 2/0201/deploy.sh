dd39c0b15dcf911bb7aa360be5a04522d5323aab
vynu <vynu@live.com>
2017-09-10 19:00:28 -0400

    modified cloud machine address modified docker image name

--- Real World Docker - Working Files/Chapter 2/0201/deploy.sh
+++ Real World Docker - Working Files/Chapter 2/0201/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-docker build -t vynu/sample-node .
-docker push vynu/sample-node
+docker build -t vynu/sample-node-old .
+docker push vynu/sample-node-old
 
-ssh deploy@docker.digocn << EOF
-docker pull vynu/sample-node:latest
+ssh vynu@aws.fedora << EOF
+docker pull vynu/sample-node-old:latest
 docker stop web || true
 docker rm web || true
-docker rmi vynu/sample-node:current || true
-docker tag vynu/sample-node:latest vynu/sample-node:current
-docker run -d --net app --restart always --name web -p 3000:3000 vynu/sample-node:current
+docker rmi vynu/sample-node-old:current || true
+docker tag vynu/sample-node-old:latest vynu/sample-node-old:current
+docker run -d --restart always --name web -p 3000:3000 vynu/sample-node-old:current
 EOF
